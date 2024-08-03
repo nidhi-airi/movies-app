193c1fa660f95295c7a3dd118dc59ae89d6a20f6
Nidhi <vfx@gmail.com>
2024-08-03 16:58:12 +0530

    banner functionality with dynamic data

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,14 +1,41 @@
 import React from 'react'
+import axios from "axios" 
+import { useState, useEffect } from 'react' 
 
+const BASE_URL="https://image.tmdb.org/t/p/original"
 const Banner = () => {
-  return (
-    <div className='relative'>
-        <img className='h-[52rem] w-full object-cover' src="https://image.tmdb.org/t/p/original/sRLC052ieEzkQs9dEtPMfFxYkej.jpg"></img>
-        <div className='absolute bottom-0 text-white bg-black bg-opacity-50 w-full p-4 text-center text-lg'>
-            <p>Rebel Moon-Part One: A Child of Fire</p>
-        </div>
-    </div>
-  )
+    const [data,setData] = useState({})
+    let url="https://api.themoviedb.org/3/trending/movie/day?api_key=2634e9f079c604567d18059d526b4346"
+    
+   
+    const fetch_data=()=>{
+        axios.get(url)
+        .then((res)=>setData(res.data.results))}
+    
+    useEffect(()=>{
+        fetch_data(url)
+    },[])
+    let idx=Math.floor(Math.random()*20)
+
+   
+ 
+    if(data){
+        return (
+            <div className='relative'>
+                <img className='h-[55rem] w-full object-cover' src={BASE_URL+data[idx]?.poster_path} ></img>
+                <div className='absolute bottom-0 text-white bg-black bg-opacity-50 w-full p-4 text-center text-lg'>
+                    <p>{data[idx]?.title}</p>
+                </div>
+                
+            </div>
+        )
+    }
+    else{
+        return(
+            <p>Loading</p>
+        )
+    }
+
 }
 
 export default Banner
--- src/Components/Movies.jsx
+++ src/Components/Movies.jsx
@@ -1,57 +1,16 @@
 import React from 'react'
 
+
 const Movies = () => {
+
   return (
     <div className='bg-gray-200 flex flex-wrap justify-evenly gap-10 mt-10 p-10'>
      <div className='relative' >
         <img className='h-[25rem] w-[18rem] rounded-xl' src="https://image.tmdb.org/t/p/original//wsgni4E1UnBuvFouUUanSdniQi0.jpg"/>
         <div className='absolute bottom-0 p-3 tracking-wide text-lg  text-center rounded-xl bg-black bg-opacity-50 text-white w-[18rem]'>The Fall Guy</div>
      </div>
-     <div className='relative' >
-        <img className='h-[25rem] w-[18rem] rounded-xl' src="https://image.tmdb.org/t/p/original//wsgni4E1UnBuvFouUUanSdniQi0.jpg"/>
-        <div className='absolute bottom-0 p-3 tracking-wide text-lg  text-center rounded-xl bg-black bg-opacity-50 text-white w-[18rem]'>The Fall Guy</div>
-     </div>
-     <div className='relative' >
-        <img className='h-[25rem] w-[18rem] rounded-xl' src="https://image.tmdb.org/t/p/original//wsgni4E1UnBuvFouUUanSdniQi0.jpg"/>
-        <div className='absolute bottom-0 p-3 tracking-wide text-lg  text-center rounded-xl bg-black bg-opacity-50 text-white w-[18rem]'>The Fall Guy</div>
-     </div>
-     <div className='relative' >
-        <img className='h-[25rem] w-[18rem] rounded-xl' src="https://image.tmdb.org/t/p/original//wsgni4E1UnBuvFouUUanSdniQi0.jpg"/>
-        <div className='absolute bottom-0 p-3 tracking-wide text-lg  text-center rounded-xl bg-black bg-opacity-50 text-white w-[18rem]'>The Fall Guy</div>
-     </div>
-     <div className='relative' >
-        <img className='h-[25rem] w-[18rem] rounded-xl' src="https://image.tmdb.org/t/p/original//wsgni4E1UnBuvFouUUanSdniQi0.jpg"/>
-        <div className='absolute bottom-0 p-3 tracking-wide text-lg  text-center rounded-xl bg-black bg-opacity-50 text-white w-[18rem]'>The Fall Guy</div>
-     </div>
-     <div className='relative' >
-        <img className='h-[25rem] w-[18rem] rounded-xl' src="https://image.tmdb.org/t/p/original//wsgni4E1UnBuvFouUUanSdniQi0.jpg"/>
-        <div className='absolute bottom-0 p-3 tracking-wide text-lg  text-center rounded-xl bg-black bg-opacity-50 text-white w-[18rem]'>The Fall Guy</div>
-     </div>
-     <div className='relative' >
-        <img className='h-[25rem] w-[18rem] rounded-xl' src="https://image.tmdb.org/t/p/original//wsgni4E1UnBuvFouUUanSdniQi0.jpg"/>
-        <div className='absolute bottom-0 p-3 tracking-wide text-lg  text-center rounded-xl bg-black bg-opacity-50 text-white w-[18rem]'>The Fall Guy</div>
-     </div>
-     <div className='relative' >
-        <img className='h-[25rem] w-[18rem] rounded-xl' src="https://image.tmdb.org/t/p/original//wsgni4E1UnBuvFouUUanSdniQi0.jpg"/>
-        <div className='absolute bottom-0 p-3 tracking-wide text-lg  text-center rounded-xl bg-black bg-opacity-50 text-white w-[18rem]'>The Fall Guy</div>
-     </div>
-     <div className='relative' >
-        <img className='h-[25rem] w-[18rem] rounded-xl' src="https://image.tmdb.org/t/p/original//wsgni4E1UnBuvFouUUanSdniQi0.jpg"/>
-        <div className='absolute bottom-0 p-3 tracking-wide text-lg  text-center rounded-xl bg-black bg-opacity-50 text-white w-[18rem]'>The Fall Guy</div>
-     </div>
-     <div className='relative' >
-        <img className='h-[25rem] w-[18rem] rounded-xl' src="https://image.tmdb.org/t/p/original//wsgni4E1UnBuvFouUUanSdniQi0.jpg"/>
-        <div className='absolute bottom-0 p-3 tracking-wide text-lg  text-center rounded-xl bg-black bg-opacity-50 text-white w-[18rem]'>The Fall Guy</div>
-     </div>
-     <div className='relative' >
-        <img className='h-[25rem] w-[18rem] rounded-xl' src="https://image.tmdb.org/t/p/original//wsgni4E1UnBuvFouUUanSdniQi0.jpg"/>
-        <div className='absolute bottom-0 p-3 tracking-wide text-lg  text-center rounded-xl bg-black bg-opacity-50 text-white w-[18rem]'>The Fall Guy</div>
-     </div>
-     <div className='relative' >
-        <img className='h-[25rem] w-[18rem] rounded-xl' src="https://image.tmdb.org/t/p/original//wsgni4E1UnBuvFouUUanSdniQi0.jpg"/>
-        <div className='absolute bottom-0 p-3 tracking-wide text-lg  text-center rounded-xl bg-black bg-opacity-50 text-white w-[18rem]'>The Fall Guy</div>
-     </div>
-     
+
+
     
         
 
